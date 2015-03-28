1d8ca844b346cf30311e78b54dc775cc6c9a9f3d
Lizzie Mamantov <mamantov@umich.edu>
2015-03-28 19:12:42 -0400

    Receives status messages

--- src/robot_main.cpp
+++ src/robot_main.cpp
@@ -1,6 +1,21 @@
-#include "ProbCogArm.h"
 #include <lcm/lcm-cpp.hpp>
 #include <iostream>
+
+#include "ProbCogArm.h"
+#include "dynamixel_status_list_t.hpp"
+
+class lcm_handler
+{
+public:
+    ~lcm_handler() {};
+
+    void handle_message(const lcm::ReceiveBuffer* rbuf,
+                        const std::string& channel,
+                        const dynamixel_status_list_t* stats)
+    {
+        std::cout << "Recieved message" << std::endl;
+    }
+};
 
 int main(int argc, char* argv[])
 {
@@ -10,6 +25,12 @@ int main(int argc, char* argv[])
         std::cout << "Failed to initialize LCM." << std::endl;
         return 1;
     }
+
+    lcm_handler handler;
+    lcm.subscribe("ARM_STATUS", &lcm_handler::handle_message,
+                  &handler);
+
+    while(0 == lcm.handle());
 
     return 0;
 }
