88c9718718befafb96b870bb6d73b6f3f4831a74
Lizzie Mamantov <mamantov@umich.edu>
2015-03-28 19:32:36 -0400

    Trying to publish commands, but not right. Pushing so I can work at home.

--- src/robot_main.cpp
+++ src/robot_main.cpp
@@ -1,8 +1,10 @@
 #include <lcm/lcm-cpp.hpp>
 #include <iostream>
+#include <unistd.h>
 
 #include "ProbCogArm.h"
 #include "dynamixel_status_list_t.hpp"
+#include "dynamixel_command_list_t.hpp"
 
 class lcm_handler
 {
@@ -30,7 +32,22 @@ int main(int argc, char* argv[])
     lcm.subscribe("ARM_STATUS", &lcm_handler::handle_message,
                   &handler);
 
-    while(0 == lcm.handle());
+    while(0 == lcm.handle())
+    {
+        dynamixel_command_list_t command;
+        command.len = probcog_arm::get_num_joints();
+        for (int i = 0; i < probcog_arm::get_num_joints(); i++)
+        {
+            dynamixel_command_t c;
+            c.position_radians = 0.1;
+            c.speed = probcog_arm::get_default_speed(i);
+            c.max_torque = probcog_arm::get_default_torque(i);
+            command.commands.push_back(c);
+        }
 
-    return 0;
+        lcm.publish("ARM_COMMANDS", &command);
+        sleep(0.1);
+    }
+
+return 0;
 }
