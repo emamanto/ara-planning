adc6dcfac748ae82c32cb5a17cb255b3a032ada5
Lizzie Mamantov <mamantov@umich.edu>
2015-03-31 16:51:05 -0400

    Attempt to control probcog arm with a whole plan. Segfaulting, though. UGH.

--- src/robot_main.cpp
+++ src/robot_main.cpp
@@ -12,19 +12,35 @@
 class lcm_handler
 {
 public:
-    lcm_handler() : current_command(5,M_PI/6) {};
+    lcm_handler() : current_command(probcog_arm::get_num_joints(), 0) {};
     ~lcm_handler() {};
 
     void handle_status_message(const lcm::ReceiveBuffer* rbuf,
                                const std::string& channel,
                                const dynamixel_status_list_t* stats)
     {
+        if (searching) return;
         pose np;
         for (int i = 0; i < probcog_arm::get_num_joints(); i++)
         {
             np.push_back(stats->statuses[i].position_radians);
         }
         if (np != status) status = np;
+
+        bool done = true;
+        for (int i = 0; i < probcog_arm::get_num_joints(); i++)
+        {
+            if (fabs(status[i] - current_command[i]) > 0.01)
+            {
+                done = false;
+                break;
+            }
+        }
+        if (done && current_plan.size() > 0)
+        {
+            current_command_index++;
+            current_command = current_plan[current_command_index];
+        }
 
         dynamixel_command_list_t command;
         command.len = probcog_arm::get_num_joints() + 1;
@@ -51,62 +67,52 @@ public:
                                const std::string& channel,
                                const search_target_t* targ)
     {
+        searching = true;
         lcm::LCM lcm;
         point_3d goal;
         for (int i = 0; i < 3; i++)
         {
             goal.push_back(targ->target[i]);
         }
-
-        current_command = probcog_arm::apply(status,
-                                             probcog_arm::solve_ik(status, goal));
+        arm_state::target = goal;
+        std::vector<search_result<arm_state, action> > latest_search;
+        bool kill_search = false;
+        arastar<arm_state, action>(&latest_search,
+                                   &kill_search,
+                                   arm_state(status),
+                                   probcog_arm::big_primitives(),
+                                   probcog_arm::small_primitives(),
+                                   100.f);
+        current_plan = latest_search.at(latest_search.size()-1).path;
+        current_command = current_plan.at(0);
+        current_command_index = 0;
+        searching = false;
     }
 
     pose status;
     pose current_command;
+    int current_command_index;
+    bool searching;
+    std::vector<pose> current_plan;
 };
 
 int main(int argc, char* argv[])
 {
-    // lcm::LCM lcm;
-    // if (!lcm.good())
-    // {
-    //     std::cout << "Failed to initialize LCM." << std::endl;
-    //     return 1;
-    // }
+    lcm::LCM lcm;
+    if (!lcm.good())
+    {
+        std::cout << "Failed to initialize LCM." << std::endl;
+        return 1;
+    }
 
     probcog_arm::INIT();
-    // lcm_handler handler;
-    // lcm.subscribe("ARM_STATUS", &lcm_handler::handle_status_message,
-    //               &handler);
-    // lcm.subscribe("SEARCH_TARGET", &lcm_handler::handle_target_message,
-    //               &handler);
+    lcm_handler handler;
+    lcm.subscribe("ARM_STATUS", &lcm_handler::handle_status_message,
+                  &handler);
+    lcm.subscribe("SEARCH_TARGET", &lcm_handler::handle_target_message,
+                  &handler);
 
-    // while(0 == lcm.handle());
-
-    pose p = pose(5, 0);
-    point_3d target = probcog_arm::ee_xyz(p);
-    std::cout << "Starting at: " << target[0] << ", " << target[1]
-              << ", "  << target[2] << std::endl;
-    target[1] += 0.05;
-    target[2] -= 0.1;
-    arm_state::target = target;
-
-    std::cout << "Going to: " << target[0] << ", " << target[1]
-              << ", "  << target[2] << std::endl;
-
-    std::cout << "Using " << probcog_arm::big_primitives().size()
-              << " big and " << probcog_arm::small_primitives().size()
-              << " small" << std::endl;
-
-    std::vector<search_result<arm_state, action> > res;
-    bool kill_search = false;
-    arastar<arm_state, action>(&res,
-                               &kill_search,
-                               arm_state(p),
-                               probcog_arm::big_primitives(),
-                               probcog_arm::small_primitives(),
-                               100.f);
+    while(0 == lcm.handle());
 
     return 0;
 }
