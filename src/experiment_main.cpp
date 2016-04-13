b75e08ca2392a191229579b15d2c02b68b1cb4c8
Lizzie Mamantov <mamantov@umich.edu>
2016-04-13 18:02:15 -0400

    Ooops again.

--- src/experiment_main.cpp
+++ src/experiment_main.cpp
@@ -497,12 +497,14 @@ void experiment_handler::compute_next_plan()
         std::vector<pose> plan_in_poses;
         if (planning_algo == RRTSTAR)
         {
-            plan_in_poses = rrtplanners::plan(apos, end_pose, time_lim, false);
+            plan_in_poses = rrtplanners::plan(apos, end_pose,
+                                              search_time_lim, false);
             plan_in_poses.push_back(end_pose);
         }
         else
         {
-            plan_in_poses = rrtplanners::plan(apos, end_pose, time_lim, true);
+            plan_in_poses = rrtplanners::plan(apos, end_pose,
+                                              search_time_lim, true);
         }
         current_plan = convert(plan_in_poses);
     }
